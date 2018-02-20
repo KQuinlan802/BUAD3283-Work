495248a02ef1aa8b9ce62fa3e65b8e90f13b0e6f
Kaerstin <kaekae02@gmail.com>
2018-02-20 14:44:35 -0600

    Added navigation elements

--- Assignments/February_15/index.php
+++ Assignments/February_15/index.php
@@ -1,14 +1,19 @@
 <HTML>
+<head>
+  <title>Masterhead and Navigation</title>
+</head>
   <body>
+    <link rel="stylesheet" href="style.css">
     <div class="masthead">
       <div class="masthead-logo"></div>
     </div>
     <div class="navigation">
       <ul class="navigation-group">
         <li class="navigation-item">Home</li>
-        <li class="navigation-item">About</li>
-        <li class="navigation-item">Contact</li>
+        <li class="navigation-item">About Us</li>
+        <li class="navigation-item">Contact Us</li>
       </ul>
     </div>
   </body>
+
 </HTML>
