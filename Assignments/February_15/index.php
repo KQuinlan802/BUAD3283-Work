532466f00a654eb38c4b811eec50a1b33dd0c96a
Kaerstin <kaekae02@gmail.com>
2018-02-27 15:11:45 -0600

    Added to Feb 16 assignment

--- Assignments/February_15/index.php
+++ Assignments/February_15/index.php
@@ -1,22 +1,15 @@
-<?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    $content = 'home';
-    if (isset($_GET['p']) && file_exists('pages/'. $_GET['p'] . '.php')) {
-       $content = $_GET['p'];
-    } elseif (isset($_GET['p']) && !file_exists('pages/'. $_GET['p'] . '.php')) {
-        $content = '404';
-    }
-?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
-        <div class="masthead"><?php include 'modules/masthead.php'; ?></div>
-        <div class="navigation"><?php include 'modules/navigation.php'; ?></div>
-
-    </body>
+<head>
+<title>Navigation</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="sidenav">
+	<a href="index.php">Home</a>
+    <a href="aboutus.php">About us</a>
+    <a href="contact.php">Contact Us</a>
+  </div>
+</body>
 </html>
