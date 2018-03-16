14aeabb5cec4d1b9cb4b04feb87032a3ffce25c0
Nate Nolting <me@natenolting.com>
2018-03-16 10:27:37 -0500

    Tweaks to February 15th Assignment
    Here are some fixes I made for your assignment from February 15th. I put comments where elements might have been confusing. I wanted to make sure you were getting how to modularize your projects.

--- Assignments/February_15/index.php
+++ Assignments/February_15/index.php
@@ -1,15 +1,33 @@
+<?php
+// load in a bootstrap file
+require_once __DIR__ . '/modules/bootstrap.php';
 
+    // now that we have the $request variable created in modules/bootstrap.php
+    // we can look for that item in the nav and get the correct page from it
+    if (array_key_exists('p', $request) && in_array($request['p'], $navItems) && file_exists(__DIR__ . '/'. $request['p'].'.php')) {
+        $page = $request['p'];
+    } else {
+        $page = 'home';
+    }
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Navigation</title>
+<title><?php echo  $navTitles[array_search($page, $navItems)]; ?></title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="sidenav">
+<?php echo $navigation; ?>
+	<!--
+	No need for these any more.
 	<a href="index.php">Home</a>
     <a href="aboutus.php">About us</a>
-    <a href="contact.php">Contact Us</a>
+    <a href="contact.php">Contact Us</a> -->
   </div>
+  <?php
+    include_once __DIR__ . '/' . $page . '.php';
+  ?>
 </body>
 </html>
